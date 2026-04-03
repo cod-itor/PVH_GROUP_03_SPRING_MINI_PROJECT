34d46a8980b6a1c3d9deaeba5122b6a0a5543878
Sathimmo <sathim35@gmail.com>
2026-04-04 00:01:46 +0700

    file-upload-successfully

--- src/main/java/org/ksga/pvh_group_03_spring_mini_project/service/S3FileService.java
+++ src/main/java/org/ksga/pvh_group_03_spring_mini_project/service/S3FileService.java
@@ -1,6 +1,14 @@
 package org.ksga.pvh_group_03_spring_mini_project.service;
 
 
+import jakarta.validation.Valid;
+import org.ksga.pvh_group_03_spring_mini_project.model.entity.FileMetadata;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface S3FileService {
 
+    FileMetadata uploadFile(@Valid MultipartFile file);
+
+    Resource getFileByFileName(@Valid String fileName);
 }
