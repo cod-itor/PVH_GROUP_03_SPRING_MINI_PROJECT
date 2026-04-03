03f8c3996816153ef4d80fcc324b2f52bfb676a4
sothearah <sothearah168@gmail.com>
2026-04-04 02:33:06 +0700

    Achievement completed successfully

--- src/main/java/org/ksga/pvh_group_03_spring_mini_project/controller/AchievementController.java
+++ src/main/java/org/ksga/pvh_group_03_spring_mini_project/controller/AchievementController.java
@@ -1,10 +1,20 @@
 package org.ksga.pvh_group_03_spring_mini_project.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.ksga.pvh_group_03_spring_mini_project.model.entity.Achievement;
+import org.ksga.pvh_group_03_spring_mini_project.model.response.ApiResponse;
 import org.ksga.pvh_group_03_spring_mini_project.service.AchievementService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 
 @RestController
@@ -14,5 +24,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class AchievementController {
 
     private final AchievementService achievementService;
+
+    @Operation(summary = "Get user profile", description = "Fetches the details of the currently authenticated user.")
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Achievement>>> getAllAchievement (@RequestParam(defaultValue = "1")
+                                                                                 @Min(value = 1, message = "Page number must be greater than 0") Integer page,
+                                                                             @RequestParam(defaultValue = "10")
+                                                                                 @Min(value = 1, message = "Page size must be greater than 0") Integer size) {
+        ApiResponse<List<Achievement>> apiResponse = ApiResponse.<List<Achievement>>builder()
+                .success(true)
+                .message("Achievements retrieved successfully")
+                .status(HttpStatus.OK)
+                .payload(achievementService.getAllAchievement(page, size))
+                .timestamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get user achievements", description = "Fetches achievements earned by the currently authenticated user")
+    @GetMapping("/{app-users}")
+    public ResponseEntity<ApiResponse<List<Achievement>>> getUserAchievements(@RequestParam(defaultValue = "1")
+                                                                              @Min(value = 1, message = "Page number must be greater than 0") Integer page,
+                                                                              @RequestParam(defaultValue = "10")
+                                                                              @Min(value = 1, message = "Page size must be greater than 0") Integer size) {
+        ApiResponse<List<Achievement>> apiResponse = ApiResponse.<List<Achievement>>builder()
+                .success(true)
+                .message("Achievements for the specified App User retrieved successfully!")
+                .status(HttpStatus.OK)
+                .payload(achievementService.getUserAchievements(page, size))
+                .timestamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
 
 }
--- src/main/java/org/ksga/pvh_group_03_spring_mini_project/service/impl/AchievementServiceImpl.java
+++ src/main/java/org/ksga/pvh_group_03_spring_mini_project/service/impl/AchievementServiceImpl.java
@@ -1,11 +1,20 @@
 package org.ksga.pvh_group_03_spring_mini_project.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.ksga.pvh_group_03_spring_mini_project.exception.NotFoundException;
 import org.ksga.pvh_group_03_spring_mini_project.helper.AuthUtils;
+import org.ksga.pvh_group_03_spring_mini_project.model.entity.Achievement;
+import org.ksga.pvh_group_03_spring_mini_project.model.entity.AppUser;
+import org.ksga.pvh_group_03_spring_mini_project.model.response.ApiResponse;
+import org.ksga.pvh_group_03_spring_mini_project.model.response.AppUserResponse;
 import org.ksga.pvh_group_03_spring_mini_project.repository.AchievementRepository;
 import org.ksga.pvh_group_03_spring_mini_project.repository.AppUserRepository;
 import org.ksga.pvh_group_03_spring_mini_project.service.AchievementService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +23,27 @@ public class AchievementServiceImpl implements AchievementService {
     private final AchievementRepository achievementRepository;
     private final AuthUtils authUtils;
     private final AppUserRepository appUserRepository;
+
+    @Override
+    public List<Achievement> getAllAchievement(Integer page, Integer size) {
+
+        int offset =  size * (page - 1);
+        return achievementRepository.getAllAchievement(offset, size);
+    }
+
+    @Override
+    public List<Achievement> getUserAchievements(Integer page, Integer size) {
+
+        int offset = size * (page - 1);
+        UUID appUserId = authUtils.getCurrentUserIdentifier();
+
+        AppUser appUser = appUserRepository.findUserByUUID(appUserId);
+
+        if (appUser == null) {
+            throw new NotFoundException("User not found!");
+        }
+
+        return achievementRepository.getUserAchievements(appUserId, offset, size);
+    }
 
 }
